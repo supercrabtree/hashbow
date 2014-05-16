d874bf37161008bd59385ecac6d9cfcf09d9ad46
George Crabtree <supercrabtree@googlemail.com>
2014-05-16 19:38:15 +1000

    added boolean and array tests

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,13 @@
 var expect = require('chai').expect
   , hashbow = require('../index.js');
 
+
+var person = {
+  name: 'Randy Smasher',
+  job: 'Stripper',
+  banana: true
+};
+
 describe('Hashbow', function () {
   it('should turn a String into a hex value', function () {
 
@@ -14,12 +21,6 @@ describe('Hashbow', function () {
   });
 
   it('should turn an Object into a hex value', function () {
-
-    var person = {
-      name: 'Randy Smasher',
-      job: 'Stripper',
-      banana: true
-    }
 
     var results = hashbow(person, 100, 50);
 
@@ -43,7 +44,18 @@ describe('Hashbow', function () {
 
   it('should turn a Boolean into a hex value', function () {
 
-    var results = hashbow(true, 100, 50);
+    var results = hashbow(false, 100, 50);
+
+    expect(results).to.be.a('string');
+    expect(results.charAt(0)).to.equal('#');
+    expect(results).to.have.length.within(4, 7);
+    expect(results).not.to.have.length(5);
+    expect(results).not.to.have.length(6);
+  });
+
+  it('should turn a Array into a hex value', function () {
+
+    var results = hashbow(['face', person, 90210], 100, 50);
 
     expect(results).to.be.a('string');
     expect(results.charAt(0)).to.equal('#');
