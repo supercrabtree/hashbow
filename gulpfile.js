d515591af0e3103358e366d2d7867243f09f8d79
supercrabtree <supercrabtree@googlemail.com>
2014-05-19 10:38:20 +1000

    hyper sexy gulp-stylus errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp')
+  , gutil = require('gulp-util')
+  , prefix = require('gulp-autoprefixer')
   , express = require('express')
   , stylus = require('gulp-stylus');
 
@@ -6,7 +8,10 @@ var lr;
 gulp.task('stylus', function () {
   return gulp.src('src/style.styl')
     .pipe(stylus())
-    .pipe(gulp.dest('.'));
+    .on('error', stylusErr)
+    .pipe(prefix('last 2 versions'))
+    .on('error', err)
+    .pipe(gulp.dest('.'))
 });
 
 gulp.task('startServers', function (cb) {
@@ -20,6 +25,25 @@ gulp.task('startServers', function (cb) {
   lr.listen(35729);
   cb();
 });
+
+function stylusErr(err) {
+  var splitErr = err.message.match(/(.*?)([0-9]*)(?:\n)((?:.+\n)+)(?:\n)((?:.+\n)+)/);
+  var file = splitErr[1];
+  var line = splitErr[2];
+  var location = splitErr[3];
+  var theErr = splitErr[4];
+
+  console.log('\n', gutil.colors.cyan(file) + gutil.colors.red(line));
+  console.log(location); 
+  console.log(gutil.colors.red(theErr));
+  gutil.beep();
+  this.emit('end');
+}
+
+function err() {
+  gutil.beep();
+  this.emit('end');
+}
 
 function notifyLivereload(event) {
   gulp.src(event.path, {read: false})
